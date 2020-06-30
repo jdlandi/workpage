645e2c43e060ae81a7c31d3d1617b340c9135699
Juan <jdlandi@cps.edu>
2020-06-30 11:15:06 -0500

    first commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from '../assets/mock-heroes';
+
+import { Observable, of } from 'rxjs';
+
+import { MessagesService } from './messages.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(
+    private messagesService: MessagesService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messagesService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messagesService.add(`HeroService: fethed hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+}
